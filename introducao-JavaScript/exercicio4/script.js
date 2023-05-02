180bc40154f4df4aad3ee47912bb6e5961700331
Andrieli Alves <andrielidesouzaalves@hotmail.com>
2023-05-02 20:35:36 -0300

    Fazendo o exercicio 4

---
+++ introducao-JavaScript/exercicio4/script.js
@@ -0,0 +1,18 @@
+let currentHour= 18;
+let message = "";
+if (currentHour >= 22){
+  console.log("Não deveríamos comer nada, é hora de dormir");
+  }
+
+  else if (currentHour >= 18 && mcurrentHour < 22){
+    message ="Rango da noite, vamos jantar :D";
+  }
+  else if (currentHour >= 14 && currentHour < 18){
+    message ="Vamos fazer um bolo pro café da tarde?";
+  }
+  else if (currentHour >=  11 && currentHour <14){
+    message ="HOra do almoço!!!";
+  }
+  else if (currentHour >= 4 && currentHour < 11){
+    message ="Hmmm, cheiro de café recém-passado";
+  }
